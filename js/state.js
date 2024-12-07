21cb7fd65d3ede585d001d03be25d2c53499070c
sosegadamente <149854564+sosegadamente@users.noreply.github.com>
2024-12-07 22:49:37 +0900

    update
    update state rendering

--- js/state.js
+++ js/state.js
@@ -1,7 +1,8 @@
 const todoApp = () => {
   let state = JSON.parse(localStorage.getItem('todos')) ?? [];
+  
   function setStorage(newState) {
-    state = { ...state, ...newState };
+    state = { ...structuredClone(state), ...newState };
     localStorage.setItem('todos', JSON.stringify(state));
     renderController();
   }
@@ -10,19 +11,26 @@ const todoApp = () => {
     renderController();
   }
 
-  function todoFormmater(value) {
-    return { isFinished: false, value, idx: Math.random() };
-  }
-
   function addTodo({ keyCode }) {
     const $newTodo = $('.new-todo');
     if (keyCode !== 13 || !$newTodo.value.trim()) return;
-    setStorage({ todos: [...state.todos, {id: todoFormmater($newTodo.value)}] });
+    setStorage({ todos: [...state.todos, { isFinished: false, value: $newTodo.value, idx: Math.random() }] });
     $newTodo.value = "";
   }
 
-  function renderController() {
-
+  function renderController(todoList = state.todos) {
+    todoList.forEach((todoItem) => {
+      const $li = document.createElement('li');
+      if (todoItem.isFinished === true) $li.classList.add('completed');
+      $li.innerHTML = `
+        <div class="view" data-idx=${todoItem.idx}>
+          <input class="toggle" type="checkbox" ${todoItem.isFinished === true ? 'checked' : ''} />
+          <label>${todoItem.value}</label>
+          <button class="destroy"></button>
+        </div>
+      `;
+      $('.todo-list').append($li);
+    });
   }
 
   function setEvents() {

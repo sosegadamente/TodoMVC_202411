8ecf402010e2e6fba6556ff81bab5a0ea40192a1
sosegadamente <149854564+sosegadamente@users.noreply.github.com>
2024-12-24 22:29:46 +0900

    update
    made dom vars

--- js/third.js
+++ js/third.js
@@ -1,3 +1,11 @@
+const $footer = $(".footer");
+const $todoInput = $(".new-todo");
+const $todoList = $(".todo-list");
+const $toggleAll = $(".toggle-all");
+const $todoCount = $(".todoCount");
+const $clearCompleted = $(".clear-completed");
+const $filterItems = $$(".filters a");
+
 
 
 class TodoApp{

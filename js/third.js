b68cb7606ea133ba670590c42f6ab0ce8a991eaf
sosegadamente <uijuncoding@gmail.com>
2024-12-25 11:43:22 +0900

    update

--- js/third.js
+++ js/third.js
@@ -1,17 +1,39 @@
-const $footer = $(".footer");
-const $todoInput = $(".new-todo");
-const $todoList = $(".todo-list");
-const $toggleAll = $(".toggle-all");
-const $todoCount = $(".todoCount");
-const $clearCompleted = $(".clear-completed");
-const $filterItems = $$(".filters a");
+const todoApp = () => {
+  const $footer = $(".footer");
+  const $todoInput = $(".new-todo");
+  const $todoList = $(".todo-list");
+  const $toggleAll = $(".toggle-all");
+  const $todoCount = $(".todoCount");
+  const $clearCompleted = $(".clear-completed");
+  const $filterItems = $$(".filters a");
 
+  const getStorage = () => { return JSON.parse(localStorage.getItem("todos")) ?? { todos: [] }; };
+  const setStorage = (todo) => { localStorage.setItem("todos", JSON.stringify(todo)); };
+  let state = getStorage();
 
-
-class TodoApp{
-  constructor() {
-
+  class TodoServices {
+    createTodo(value) {
+      return { idx: crypto.randomUUID(), isFinished: false, value };
+    }
+    addTodo(value) {
+      const todos = [...state.todos, this.createTodo(value)];
+      console.log(...state.todos);
+      console.log(this.createTodo(value));
+      setStorage({ todos });
+    }
   }
-}
+  
+  const todoServices = new TodoServices();
 
-new TodoApp();
+  function renderCtrl(renderItem) {
+    console.log(renderItem);
+  }
+
+  function eventCtrl () {
+    $todoInput.onkeydown = (e) => { if (e.keyCode === 13 && e.target.value.trim()) todoServices.addTodo(e.target.value); }
+  }
+
+  eventCtrl();
+};
+
+todoApp();
